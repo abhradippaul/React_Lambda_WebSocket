204429a68bc3a1e1a1667ddd21312ec3e6492b43
Abhradip Paul <abhradipserampore@gmail.com>
2025-12-08 23:36:21 +0530

    Lambda and dynamodb setup done.

--- terraform/module/lambda/main.tf
+++ terraform/module/lambda/main.tf
@@ -1,7 +1,19 @@
 data "archive_file" "connect_handler_zipped_file" {
   type        = "zip"
   source_file = "${path.module}/functions/connect_handler.js"
-  output_path = "${path.module}/zipped-function/connect_handler.zip"
+  output_path = "${path.module}/zipped-functions/connect_handler.zip"
+}
+
+data "archive_file" "disconnect_handler_zipped_file" {
+  type        = "zip"
+  source_file = "${path.module}/functions/disconnect_handler.js"
+  output_path = "${path.module}/zipped-functions/disconnect_handler.zip"
+}
+
+data "archive_file" "sendmessage_handler_zipped_file" {
+  type        = "zip"
+  source_file = "${path.module}/functions/sendMessage_handler.js"
+  output_path = "${path.module}/zipped-functions/sendMessage_handler.zip"
 }
 
 data "aws_iam_policy_document" "assume_policy_document" {
@@ -30,6 +42,23 @@ data "aws_iam_policy_document" "dynamodb_handler_policy_document" {
   }
 }
 
+data "aws_iam_policy_document" "sendmessage_handler_policy_document" {
+  statement {
+    actions = [
+      "dynamodb:BatchGetItem",
+      "dynamodb:GetRecords",
+      "dynamodb:GetShardIterator",
+      "dynamodb:Query",
+      "dynamodb:GetItem",
+      "dynamodb:Scan",
+      "dynamodb:ConditionCheckItem",
+      "dynamodb:DescribeTable"
+
+    ]
+    resources = [var.dynamodb_arn]
+  }
+}
+
 resource "aws_iam_policy" "dynamodb_handler_policy" {
   policy = data.aws_iam_policy_document.dynamodb_handler_policy_document.json
   name   = "dynamodb_handler_policy"
@@ -37,7 +66,7 @@ resource "aws_iam_policy" "dynamodb_handler_policy" {
 
 resource "aws_iam_role" "role_handler" {
   assume_role_policy = data.aws_iam_policy_document.assume_policy_document.json
-  name               = "ConnectHandler"
+  name               = "role_handler"
 }
 
 resource "aws_iam_role_policy_attachment" "role_handler" {
@@ -74,11 +103,11 @@ resource "aws_lambda_function" "connect_handler" {
 }
 
 resource "aws_lambda_function" "disconnect_handler" {
-  filename         = data.archive_file.connect_handler_zipped_file.output_path
+  filename         = data.archive_file.disconnect_handler_zipped_file.output_path
   function_name    = "disconnect_handler"
   role             = aws_iam_role.role_handler.arn
   handler          = "index.handler"
-  source_code_hash = data.archive_file.connect_handler_zipped_file.output_base64sha256
+  source_code_hash = data.archive_file.disconnect_handler_zipped_file.output_base64sha256
 
   runtime = "nodejs22.x"
 
@@ -93,15 +122,15 @@ resource "aws_lambda_function" "disconnect_handler" {
     Environment = var.env
   }
 
-  depends_on = [aws_iam_role.role_handler, data.archive_file.connect_handler_zipped_file]
+  depends_on = [aws_iam_role.role_handler, data.archive_file.disconnect_handler_zipped_file]
 }
 
 resource "aws_lambda_function" "sendmessage_handler" {
-  filename         = data.archive_file.connect_handler_zipped_file.output_path
-  function_name    = "disconnect_handler"
+  filename         = data.archive_file.sendmessage_handler_zipped_file.output_path
+  function_name    = "sendmessage_handler"
   role             = aws_iam_role.role_handler.arn
   handler          = "index.handler"
-  source_code_hash = data.archive_file.connect_handler_zipped_file.output_base64sha256
+  source_code_hash = data.archive_file.sendmessage_handler_zipped_file.output_base64sha256
 
   runtime = "nodejs22.x"
 
@@ -116,5 +145,5 @@ resource "aws_lambda_function" "sendmessage_handler" {
     Environment = var.env
   }
 
-  depends_on = [aws_iam_role.role_handler, data.archive_file.connect_handler_zipped_file]
+  depends_on = [aws_iam_role.role_handler, data.archive_file.sendmessage_handler_zipped_file]
 }
